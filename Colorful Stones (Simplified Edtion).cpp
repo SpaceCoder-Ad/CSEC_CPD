8d27ffe3f228507d293bde614f6daccebc262316
SpaceCoder-Ad <progcoder1@gmail.com>
2025-02-12 14:50:04 +0300

    solution for Colorful Stones (Simplified Edtion)

---
+++ Colorful Stones (Simplified Edtion).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s, t;
+
+  cin >> s >> t;
+
+  int pos = 0;
+
+  for (int i = 0; i < t.length(); ++i) {
+    if (t[i] == s[pos]) pos++;
+  }
+
+  cout << pos + 1 << endl;
+  
+  return 0;
+}
